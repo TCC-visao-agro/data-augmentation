e46c42bd194a3e3e3f166be4e66890b7cb84c04c
Alessandro <alessanbiz@gmail.com>
2022-09-12 20:33:10 -0300

    refactor: better performance for image segmentation

--- background_addition/background_addition.py
+++ background_addition/background_addition.py
@@ -1,10 +1,11 @@
+import cv2
 from gamma_correction.gamma_correction import gamma_correction
 
 import numpy as np
 from cv2 import resize, INTER_CUBIC
 
 
-def background_addition(foreground_image, background_image):
+def add_background_image(foreground_image, background_image):
     foreground_corrected = gamma_correction(foreground_image, gamma=0.6)
     background_corrected = gamma_correction(background_image, gamma=0.6)
 
@@ -12,10 +13,42 @@ def background_addition(foreground_image, background_image):
 
     bg_resized = resize(background_corrected, (width, height), interpolation=INTER_CUBIC)
 
-    for i in range(width):
-        for j in range(height):
-            pixel = foreground_corrected[j, i]
-            if np.allclose(pixel, [0, 0, 0], rtol=10, atol=10):
-                foreground_corrected[j, i] = bg_resized[j, i]
+    binary_image = find_binary_image(foreground_corrected)
 
-    return foreground_corrected
+    changed_background_image = bg_resized
+
+    changed_background_image[binary_image, :] = foreground_corrected[binary_image]
+
+    return changed_background_image
+
+
+def add_background_color(foreground_image, color):
+    foreground_corrected = gamma_correction(foreground_image, gamma=0.6)
+
+    binary_image = find_binary_image(foreground_corrected)
+
+    changed_background_image = np.full_like(foreground_corrected, color)
+
+    changed_background_image[binary_image, :] = foreground_corrected[binary_image]
+
+    return changed_background_image
+
+
+def find_binary_image(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, threshold_image = cv2.threshold(gray_image, 20, 255, cv2.THRESH_BINARY)
+
+    gray = cv2.morphologyEx(threshold_image, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11)))
+
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    large_contours = []
+    for contour in contours:
+        if cv2.contourArea(contour) > 10000:
+            large_contours.append(contour)
+
+    gray = np.uint8(np.zeros(gray.shape))
+    segmented_leaf = cv2.drawContours(gray, large_contours, -1, 255, cv2.FILLED)
+
+    return segmented_leaf.astype(bool)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from transform.transform import *
-from background_addition.background_addition import background_addition
+from background_addition.background_addition import add_background_image, add_background_color
 from files_operation.FileManagement import FileManagement
 from alive_progress import alive_bar
 
-TIMES_TO_AUGMENT = 3
+NUMBER_OF_SAMPLES = 5000
+BACKGROUND_TYPE = "COLOR"  # IMAGE | COLOR
+COLOR = [255, 255, 255]  # Conta apenas se o tipo for "COLOR"
 
 
 def main():
@@ -15,29 +17,49 @@ def main():
 
     print("Augmenting...\n")
 
-    for leaf_class in classes:
-        leaves = file_management.get_leaves(leaf_class)
+    number_of_replied_samples = 0
 
-        with alive_bar(len(leaves) * TIMES_TO_AUGMENT, force_tty=True, title=leaf_class) as bar:
-            for leaf in leaves:
-                bar.text = f"{leaf_class}"
+    # for leaf_class in classes:
+    #     leaves = file_management.get_leaves(leaf_class)
+    #
+    #     with alive_bar(NUMBER_OF_SAMPLES, force_tty=True, title=leaf_class) as bar:
+    #         while number_of_replied_samples < NUMBER_OF_SAMPLES:
+    #             for leaf in leaves:
+    #                 if number_of_replied_samples == NUMBER_OF_SAMPLES:
+    #                     continue
+    #
+    #                 bar.text = f"{leaf_class}"
+    #
+    #                 tomato_leaf = file_management.get_leaf(leaf)
+    #
+    #                 rotated_image = rotate(tomato_leaf)
+    #
+    #                 image_mounted = None
+    #
+    #                 if BACKGROUND_TYPE == "COLOR":
+    #                     flipped_image = flip(rotated_image)
+    #                     image_mounted = add_background_color(foreground_image=flipped_image, color=COLOR)
+    #
+    #                 elif BACKGROUND_TYPE == "IMAGE":
+    #                     background = file_management.get_random_bg()
+    #
+    #                     flipped_background = flip(background)
+    #
+    #                     image_mounted = add_background_image(foreground_image=rotated_image,
+    #                                                          background_image=flipped_background)
+    #
+    #                 file_management.save(image_mounted)
+    #                 bar()
+    #                 total_leaves += 1
+    #                 number_of_replied_samples += 1
+    #
+    #     number_of_replied_samples = 0
 
-                for _ in range(TIMES_TO_AUGMENT):
+    leaves = file_management.get_leaves("Tomato___Spider_mites Two-spotted_spider_mite")
 
-                    tomato_leaf = file_management.get_leaf(leaf)
+    tomato_leaf = file_management.get_leaf("db594718-ad38-4d28-969c-c4bac1b5bbe1___Com.G_SpM_FL 8443_final_masked.jpg")
 
-                    background = file_management.get_random_bg()
-
-                    background_flipped = flip(background)
-
-                    rotated_image = rotate(tomato_leaf)
-
-                    image_mounted = background_addition(foreground_image=rotated_image,
-                                                        background_image=background_flipped)
-
-                    file_management.save(image_mounted)
-                    bar()
-                    total_leaves += 1
+    image_mounted = add_background_color(foreground_image=tomato_leaf, color=COLOR)
 
     print(f"Finished data augmentation!. Total augmented = {total_leaves}")
 
